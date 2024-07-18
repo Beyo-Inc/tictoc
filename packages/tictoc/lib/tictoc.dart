729310a1f05abcf9a3dca3fe46558a0cf757524a
oikkoikk <ljjun0702@gmail.com>
2024-07-18 15:17:51 +0900

    feat: Add tictoc web

--- packages/tictoc/lib/tictoc.dart
+++ packages/tictoc/lib/tictoc.dart
@@ -1,2 +1,3 @@
 export 'src/tictoc_core/tictoc_core.dart';
-export 'src/tictoc_native/tictoc_native.dart';
+export 'src/tictoc_native/tictoc_native.dart'
+    if (dart.library.ui_web) 'src/tictoc_web/tictoc_web.dart';
