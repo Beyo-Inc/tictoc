36639065ac67bd2295017f86d80a8d19fa43441e
Lee, Jongjun <ljjun0702@gmail.com>
2024-07-18 20:58:21 +0900

    feat: Add tictoc test (#13)
    * feat: Add tictoc native test
    
    * fix: `ntpOffset` -> `offset`
    
    * refactor: Make tests compatible for both native & web

---
+++ packages/tictoc/test/tictoc_test.dart
@@ -0,0 +1,30 @@
+import 'package:test/test.dart';
+import 'package:tictoc/tictoc.dart';
+
+void main() {
+  group(
+    '[TicToc Test]',
+    () {
+      test(
+        'now()를 호출하면, offset을 반영한 현재 시간을 반환해야 한다.',
+        () {
+          const int mockOffset = 5000;
+          final TicToc sut = TicToc();
+          final DateTime localTime = DateTime.now();
+          sut.offset = mockOffset;
+
+          final Timestamp now = sut.now(localTime);
+
+          final DateTime expectedTime =
+              localTime.add(const Duration(milliseconds: mockOffset));
+
+          expect(
+            now.dateTime,
+            expectedTime,
+            reason: 'offset이 반영된 시간을 반환해야 한다.',
+          );
+        },
+      );
+    },
+  );
+}
